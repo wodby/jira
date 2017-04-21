a088177d1b5b3e1e880a7953644d2ce1e94049a7
Chingis Sandanov <chingis@wodby.com>
2017-04-21 14:22:13 +0700

    Support confluence certificates

--- 7.3/docker-jira-entrypoint.sh
+++ 7.3/docker-jira-entrypoint.sh
@@ -8,4 +8,17 @@ fi
 
 chown daemon:daemon /var/atlassian/jira
 
+if [[ -n "${CONFLUENCE_HOST}" ]]; then
+    jre="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+
+    if [[ -z "${CONFLUENCE_PORT}" ]]; then
+        CONFLUENCE_PORT=443
+    fi
+
+    url="${CONFLUENCE_HOST}:${CONFLUENCE_PORT}"
+    openssl s_client -connect "${url}"  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/cert.pem
+    "${jre}/bin/keytool" -import -alias confluence -trustcacerts -keystore "${jre}/lib/security/cacerts" -file /tmp/cert.pem
+    rm /tmp/cert.pem
+fi
+
 gosu daemon /docker-entrypoint.sh "${@}"
