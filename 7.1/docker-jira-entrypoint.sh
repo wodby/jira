49fd51c4e3ff8d74639732e2e05d9628533ae8a6
Chingis Sandanov <chingis@wodby.com>
2017-05-15 16:42:29 +0700

    Add experimental alpine-based version

---
+++ 7.1/docker-jira-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -n "${DEBUG}" ]]; then
+  set -x
+fi
+
+chown daemon:daemon /var/atlassian/jira
+
+if [[ -n "${CONFLUENCE_HOST}" ]]; then
+    jre="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+
+    if [[ -z "${CONFLUENCE_PORT}" ]]; then
+        CONFLUENCE_PORT=443
+    fi
+
+    url="${CONFLUENCE_HOST}:${CONFLUENCE_PORT}"
+    openssl s_client -connect "${url}"  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/cert.pem
+    "${jre}/bin/keytool" -import -alias confluence -trustcacerts -keystore "${jre}/lib/security/cacerts" \
+        -file /tmp/cert.pem -storepass changeit -noprompt
+    rm /tmp/cert.pem
+fi
+
+su-exec daemon "${@}"
